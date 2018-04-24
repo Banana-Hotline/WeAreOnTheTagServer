d16453cc317b9f06a11ac777179b9f73fb22655a
GrangesKhan <nayrpg@gmail.com>
2018-04-24 01:49:47 -0400

    Add Minio DB Class for easy db interfacing

--- lazors/backend/backend.py
+++ lazors/backend/backend.py
@@ -1,5 +1,8 @@
 import logging
+from minio_db import Minio_DB
 
+db = Minio_DB()
+db.makeBucket("players")
 def get_players():
     """
     Get a list of all players
@@ -11,6 +14,9 @@ def get_players():
     """
     logging.info("Getting list of all players")
     players = []
+    player_objs = db.get_bucket_contents("players")
+    for player in player_objs:
+        players.append(get_player(player.object_name))
     return players
 
 def get_player(player_id):
@@ -23,7 +29,7 @@ def get_player(player_id):
         player
     """
     logging.info("Getting player {}".format(player_id))
-    player = {"name": "test"}
+    player = db.read_from_bucket("players", player_id)
     return player
 
 def put_player(player_id, player):
@@ -40,7 +46,12 @@ def put_player(player_id, player):
     """
     logging.info("Put player: {}".format(player_id))
     logging.info("Player info: {}".format(player))
-    player, created = ({"name": "test"}, True)
+    if(player_id != player['id']):
+        player, created = ({"name": player['name']}, False)
+        return (player, created)
+    db.makeBucket("players")
+    db.write_to_bucket("players", player_id, player)
+    player, created = ({"name": player['name']}, True)
     return (player, created)
 
 def delete_player(player_id):
@@ -53,4 +64,6 @@ def delete_player(player_id):
         player
     """
     logging.info("Deleting player {}".format(player_id))
+    db.remove_object("players", player_id)
+
     return True
